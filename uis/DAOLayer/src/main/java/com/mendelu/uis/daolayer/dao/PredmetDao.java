04ba788dcaf246d9d6a4b995d2168469c4871289
freeroman <roman@pechal.net>
2017-03-15 21:44:21 +0100

    predmet dao

---
+++ uis/DAOLayer/src/main/java/com/mendelu/uis/daolayer/dao/PredmetDao.java
@@ -0,0 +1,34 @@
+package com.mendelu.uis.daolayer.dao;
+
+import com.mendelu.uis.daolayer.domain.Predmet;
+
+import java.util.List;
+
+/**
+ * Data Access Object providing access to database operations for Predmet entity
+ *
+ * @author Roman Pechal
+ */
+public interface PredmetDao {
+
+    /**
+     * Finds and returns Predmet by given ID (primary key)
+     */
+    public Predmet findById(int id);
+
+    /**
+     * Saves Predmet entity to database, can either update existing Predmet or save new one
+     */
+    public void save(Predmet badge);
+
+    /**
+     * Deletes given Predmet entity from database
+     */
+    public void delete(Predmet badge);
+
+    /**
+     * Returns all Predmet entities from database
+     */
+    public List<Predmet> findAll();
+
+}
