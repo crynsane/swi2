24faea9588ac0464b6d1dc83798c1ad317171f4e
freeroman <address@omail.com>
2017-06-11 14:15:08 +0100

    weblayer init

---
+++ uis/ServiceLayer/src/main/java/com/mendelu/uis/servicelayer/facade/PredmetFacade.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mendelu.uis.servicelayer.facade;
+
+import com.mendelu.uis.daolayer.domain.Predmet;
+import java.util.List;
+
+/**
+ *
+ * @author pechal
+ */
+public interface PredmetFacade {
+    List<Predmet> getAllPredemt();
+}
---
+++ uis/ServiceLayer/src/main/java/com/mendelu/uis/servicelayer/facade/PredmetFacadeImpl.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mendelu.uis.servicelayer.facade;
+
+import com.mendelu.uis.daolayer.domain.Predmet;
+import com.mendelu.uis.servicelayer.service.BeanMappingService;
+import com.mendelu.uis.servicelayer.service.PredmetService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import java.util.List;
+/**
+ *
+ * @author pechal
+ */
+@Service
+@Transactional
+public class PredmetFacadeImpl implements PredmetFacade {
+
+    @Inject
+    private PredmetService predmetService;
+
+    @Inject
+    private BeanMappingService beanMappingService;
+
+    @Override
+    public List<Predmet> getAllPredemt() {
+        return beanMappingService.mapTo(predmetService.findAll(), Predmet.class);
+    }
+    
+}
